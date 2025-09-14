bd5fd8ba53a34f74d636b3a84d66d0fba3a212a7
hoverAdev <hoverdudeanimations@gmail.com>
2025-09-14 17:39:14 -0600

    Update mips64-elf-gcc.rb

--- Formula/mips64-elf-gcc.rb
+++ Formula/mips64-elf-gcc.rb
@@ -6,7 +6,7 @@ class Mips64ElfGcc < Formula
   homepage "https://gcc.gnu.org/"
   url "https://ftp.gnu.org/gnu/gcc/gcc-15.2.0/gcc-15.2.0.tar.xz"
   mirror "https://ftpmirror.gnu.org/gnu/gcc/gcc-15.2.0/gcc-15.2.0.tar.xz" 
-
+  sha256 "438fd996826b0c82485a29da03a72d71d6e3541a83ec702df4271f6fe025d24e"
   head "https://gcc.gnu.org/git/gcc.git", branch: "master"
 
   depends_on "gmp"
@@ -14,6 +14,7 @@ class Mips64ElfGcc < Formula
   depends_on "libmpc"
   depends_on "mpfr"
   depends_on "mips64-elf-binutils"
+  depends_on "make"
 
   uses_from_macos "zlib"
 
@@ -51,7 +52,7 @@ class Mips64ElfGcc < Formula
       --libdir=#{lib}/mip64-elf-gcc/#{version_suffix}
       --target=mips64-elf
       --with-arch=vr4300
-      --enable-languages=c 
+      --enable-languages=c,c++,objc,obj-c++,fortran 
       --without-headers 
       --with-newlib
       --with-gnu-as=mips64-elf-as
@@ -80,12 +81,13 @@ class Mips64ElfGcc < Formula
       --enable-plugin
       --enable-static
       --without-included-gettext
+      --with-system-zlib
     ]
 
     mkdir "build" do
       system "../configure", *args
-      system "make"
-      system "make", "install"
+      system "gmake"
+      system "gmake", "install"
     end
   end
 
